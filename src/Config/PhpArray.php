1e76f2325bbf898ef9a3b69b4ab23f625f5a0477
Zero <john.royer@gmail.com>
2017-10-01 18:07:21 +0800

    Add PHP array config reader

--- src/Config/PhpArray.php
+++ src/Config/PhpArray.php
@@ -2,6 +2,37 @@
 
 namespace Zeroplex\Tool\Config;
 
+use Zeroplex\Tool\Config;
+use Zeroplex\Tool\File;
+
 class PhpArray
 {
+    private $config = null;
+
+    public function __construct(\Zeroplex\Tool\Config $config)
+    {
+        $this->config = $config;
+    }
+
+    public function getKeyspace(string $file): string
+    {
+    }
+
+    public function loadSettings(string $file): void
+    {
+        $key = self::getKeyspace($file);
+
+        $settings = (function () use ($file) {
+            return require $file;
+        })();
+
+        if (false !== $settings) {
+            $this->config->set($key, $settings);
+        }
+    }
+
+    public function isPhpExt(string $file): bool
+    {
+        return 'php' == File::getFileExt($file);
+    }
 }
